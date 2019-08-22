6e96a2331c58aca270d4ea37b88a925179da14f7
Denys Bondarchuk <denys.bondarchuk@dataart.com>
2019-08-22 14:22:48 +0300

    add basic components and logic

---
+++ src/commponents/WeatherInfo/WeatherInfo.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import './WeatherInfo.css';
+
+const weatherInfo = ({ data }) => {
+
+  const {
+    name,
+    main,
+    weather,
+    wind
+  } = { ...data };
+
+  return (
+    <div className="info">
+      <div className="info__description">
+
+        <div className="info__row">
+          <p className="info__name">City</p>
+          <p className="info__value info__value-city">{name}</p>
+        </div>
+        <div className="info__row">
+          <p className="info__name">Temperature</p>
+          <p className="info__value info__value-temperature">{main.temp}℃</p>
+        </div>
+        <div className="info__row">
+          <p className="info__name">Humidity</p>
+          <p className="info__value info__value-humidity">{main.humidity}%</p>
+        </div>
+        <div className="info__row">
+          <p className="info__name">Wind</p>
+          <p className="info__value info__value-wind">{wind.speed} m/s</p>
+        </div>
+        <div className="info__row">
+          <p className="info__name">Description</p>
+          {
+            weather.map(item => (
+              <p
+                key={item.id}
+                className="info__value info__value-description"
+              >
+                {item.description}
+              </p>
+            ))
+          }
+        </div>
+      </div>
+      <div className="info__show">
+        <img src={`https://openweathermap.org/img/w/${weather[0].icon}.png`} alt=""/>
+      </div>
+    </div>
+  )
+}
+
+export default weatherInfo;
---
+++ src/commponents/WeatherMain/WeatherMain.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Form from '../WeatherForm/WeatherForm';
+import WeatherInfo from '../WeatherInfo/WeatherInfo';
+
+import './WeatherMain.css'
+
+class WeatherMain extends Component {
+
+	state = {
+		weather: null,
+		defaultCity: 'Kyiv'
+	};
+
+	componentDidMount() {
+		axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.defaultCity}&units=metric&appid=2ec7f7b5fab44885766bbe4fc05fde4f`)
+			.then(response => {
+				this.setState ({
+					weather: response.data
+				})	
+			}
+		)
+	}
+
+	submitHandler = (e) => {
+		e.preventDefault();
+
+		const city = (new FormData(e.target)).get('city');
+
+		axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2ec7f7b5fab44885766bbe4fc05fde4f`)
+			.then(response => {
+				this.setState({ weather: response.data });
+			})
+			.catch(err => {
+				alert('The city is not found');
+			});
+
+		e.target.reset();
+	};
+
+	render() {
+		return (
+			<div className="weather">
+				<Form
+					submitHandler={this.submitHandler}
+				/>
+				{
+					this.state.weather &&
+					<WeatherInfo
+						data={this.state.weather}
+					/>
+				}
+			</div>
+		)
+	}
+
+}
+
+export default WeatherMain;
