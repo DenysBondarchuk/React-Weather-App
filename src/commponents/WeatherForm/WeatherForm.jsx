113b8bf74d6a1914e73ef0cd11854edd4f025961
Denys Bondarchuk <denys.bondarchuk@dataart.com>
2019-08-22 16:16:54 +0300

    fixed all eslint errors

--- src/commponents/WeatherForm/WeatherForm.jsx
+++ src/commponents/WeatherForm/WeatherForm.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 
 import './WeatherForm.css';
 
-const Form = ({ submitHandler }) => {
-  return (
-    <form className="weather__form" onSubmit={(e) => { submitHandler(e); }}>
-      <input
-        type="text"
-        name="city"
-        placeholder="Write the city"
-        required
-        className="weather__input"
-      />
-      <button
-        type="submit"
-        className="weather__btn"
-      >
-        Find
-      </button>
-    </form>
-  )
-}
+const Form = ({ submitHandler }) => (
+  <form className="weather__form" onSubmit={(e) => { submitHandler(e); }}>
+    <input
+      type="text"
+      name="city"
+      placeholder="Write the city"
+      required
+      className="weather__input"
+    />
+    <button
+      type="submit"
+      className="weather__btn"
+    >
+      Find
+    </button>
+  </form>
+);
+
 
 export default Form;
--- src/commponents/WeatherMain/WeatherMain.jsx
+++ src/commponents/WeatherMain/WeatherMain.jsx
@@ -3,56 +3,52 @@ import axios from 'axios';
 import Form from '../WeatherForm/WeatherForm';
 import WeatherInfo from '../WeatherInfo/WeatherInfo';
 
-import './WeatherMain.css'
+import './WeatherMain.css';
 
 class WeatherMain extends Component {
+  state = {
+    weather: null,
+    defaultCity: 'Kyiv',
+  };
 
-	state = {
-		weather: null,
-		defaultCity: 'Kyiv'
-	};
+  componentDidMount() {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.defaultCity}&units=metric&appid=2ec7f7b5fab44885766bbe4fc05fde4f`)
+      .then((response) => {
+        this.setState({
+          weather: response.data,
+        });
+      });
+  }
 
-	componentDidMount() {
-		axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.defaultCity}&units=metric&appid=2ec7f7b5fab44885766bbe4fc05fde4f`)
-			.then(response => {
-				this.setState({
-					weather: response.data
-				})
-			})
-	}
+  submitHandler = (e) => {
+    e.preventDefault();
 
-	submitHandler = (e) => {
-		e.preventDefault();
+    const city = (new FormData(e.target)).get('city');
 
-		const city = (new FormData(e.target)).get('city');
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2ec7f7b5fab44885766bbe4fc05fde4f`)
+      .then((response) => {
+        this.setState({ weather: response.data });
+      })
+      .catch(() => {
+        // alert('The city is not found');
+      });
 
-		axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2ec7f7b5fab44885766bbe4fc05fde4f`)
-			.then(response => {
-				this.setState({ weather: response.data });
-			})
-			.catch(err => {
-				alert('The city is not found');
-			});
+    e.target.reset();
+  };
 
-		e.target.reset();
-	};
-
-	render() {
-		return (
-			<div className="weather">
-				<Form
-					submitHandler={this.submitHandler}
-				/>
-				{
-					this.state.weather &&
-					<WeatherInfo
-						data={this.state.weather}
-					/>
-				}
-			</div>
-		)
-	}
-
+  render() {
+    return (
+      <div className="weather">
+        <Form
+          submitHandler={this.submitHandler}
+        />
+        {
+          this.state.weather
+          && <WeatherInfo data={this.state.weather} />
+        }
+      </div>
+    );
+  }
 }
 
 export default WeatherMain;
