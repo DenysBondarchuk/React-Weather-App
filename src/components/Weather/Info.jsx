fabe4fba9bbc4fca1be7a58eb0d7c02ece8eaf97
Denys Bondarchuk <denys.bondarchuk@dataart.com>
2019-08-27 12:25:42 +0300

    add prop types to project

--- src/components/Weather/Info.jsx
+++ src/components/Weather/Info.jsx
@@ -12,6 +12,14 @@ const weatherInfo = ({ data }) => {
     },
   } = data;
 
+  const getDescription = (item) => (
+    <span
+      key={item.id}
+    >
+      {item.description}
+    </span>
+  );
+
   return (
     <div className="info">
       <div className="info__description">
@@ -32,53 +40,8 @@ const weatherInfo = ({ data }) => {
           m/s
         </InfoRow>
         <InfoRow className="info__value-description" heading="Description">
-          {weather.map((item) => (
-            <span
-              key={item.id}
-            >
-              {item.description}
-            </span>
-          ))}
+          {weather.map(getDescription).join(', ')}
         </InfoRow>
-
-        {/* <div className="info__row">
-          <p className="info__name">City</p>
-          <p className="info__value info__value-city">{name}</p>
-        </div>
-        <div className="info__row">
-          <p className="info__name">Temperature</p>
-          <p className="info__value info__value-temperature">
-            {temp}
-            ℃
-          </p>
-        </div>
-        <div className="info__row">
-          <p className="info__name">Humidity</p>
-          <p className="info__value info__value-humidity">
-            {humidity}
-            %
-          </p>
-        </div>
-        <div className="info__row">
-          <p className="info__name">Wind</p>
-          <p className="info__value info__value-wind">
-            {speed}
-            m/s
-          </p>
-        </div>
-        <div className="info__row">
-          <p className="info__name">Description</p>
-          {
-            weather.map((item) => (
-              <p
-                key={item.id}
-                className="info__value info__value-description"
-              >
-                {item.description}
-              </p>
-            ))
-          }
-        </div> */}
       </div>
       <div className="info__show">
         <img src={`https://openweathermap.org/img/w/${weather[0].icon}.png`} alt="" />
