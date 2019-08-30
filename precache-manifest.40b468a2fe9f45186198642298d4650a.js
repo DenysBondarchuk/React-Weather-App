dd2583d2b07b9113fabdc40f7eeffc72ecac9761
Denys Bondarchuk <denys.bondarchuk@dataart.com>
2019-08-30 13:09:33 +0300

    Updates

---
+++ precache-manifest.40b468a2fe9f45186198642298d4650a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cd412317cc821ba2fad6014889c288",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "1f5490e4767a87e0f885",
+    "url": "/React-Weather-App/static/css/main.bc51232d.chunk.css"
+  },
+  {
+    "revision": "744f34b312a196f8cc33",
+    "url": "/React-Weather-App/static/js/2.c930d71b.chunk.js"
+  },
+  {
+    "revision": "1f5490e4767a87e0f885",
+    "url": "/React-Weather-App/static/js/main.cf793ef4.chunk.js"
+  },
+  {
+    "revision": "4a84fcd6f8efc5b14e18",
+    "url": "/React-Weather-App/static/js/runtime~main.0658175d.js"
+  }
+]);
